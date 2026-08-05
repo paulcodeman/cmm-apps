2f4e8633124b2e291d9680917f893b21733d6d0e
paulcodeman <paulcodeman@users.noreply.github.com>
2026-08-05 23:29:53 +0200

    Fix Quark text line wrapping

--- quark/prepare_page.h
+++ quark/prepare_page.h
@@ -19,7 +19,7 @@ int line_end;
 
 	for (ptr = textbuf.p;    ptr < textbuf.p + textbuf.len;    ptr++)
 	{
-		if (ptr - lines.get_last() * list.font_w + 16 >= list.w)
+		if ((((ptr-lines.get_last())*list.font_w)+16) >= list.w)
 		{
 			//searching a 'white' for a normal word-break
 			for(line_end = ptr; line_end != lines.get_last(); line_end--) 			{
